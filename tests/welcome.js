f0e8a8761a74ee03cc6713b8b956dfefb89f4de7
Jason Lock <hello@jasonjlock.com>
2016-04-08 15:51:53 -0700

    Testing Travis CI Integration

--- tests/welcome.js
+++ tests/welcome.js
@@ -3,7 +3,7 @@ const dockerHost = require('docker-host')();
 module.exports = {
   'Welcome Test' : function (browser) {
     browser
-      .url('http://127.0.0.1')
+      .url(browser.launch_url)
       .waitForElementVisible('body', 500)
       .assert.containsText('h1', 'Welcome')
       .end();
