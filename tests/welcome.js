397a7cafe65086f4b1f5f2068a9df34c516a4f4a
Jason Lock <hello@jasonjlock.com>
2016-04-08 12:18:53 -0700

    Log Docker Host Settings
    Log the Docker host settings to test Saucelabs integration.

--- tests/welcome.js
+++ tests/welcome.js
@@ -2,6 +2,8 @@ const dockerHost = require('docker-host')();
 
 module.exports = {
   'Welcome Test' : function (browser) {
+    console.log(dockerHost.protocol + dockerHost.host, dockerHost);
+
     browser
       .url(dockerHost.protocol + dockerHost.host)
       .waitForElementVisible('body', 500)
